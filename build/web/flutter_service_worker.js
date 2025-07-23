5ff56617145a0504bf6b895023eb503491008334
HuzaifaShkx <huzaifayasin225@gmail.com>
2025-07-24 01:07:43 +0500

    push

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a030a63758388b045fcce977dcc41f08
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "7bc5d7af1c2170d6870e4f6c92c5e7fd",
+"flutter_bootstrap.js": "fe0f7e1bb9c9dcd967b062c8d1aff976",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
