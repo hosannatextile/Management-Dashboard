a7a9b006715cb9a7a2222054d91574dd03cfcb33
HuzaifaBrainsCraft <huzaifa.yasin@brainscraft.io>
2025-07-24 13:54:01 +0500

    deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a030a63758388b045fcce977dcc41f08
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "29344b40eccffd61f7f2f0c5b46a1540",
+"flutter_bootstrap.js": "f87a612d223179fb5b95af688b22d11b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
